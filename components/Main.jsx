028f88fcbce3eab00f2926cb600179402c544b0d
DevRys <eduardoreyes55@aragon.unam.mx>
2024-07-22 11:37:31 -0600

    Finish app: fecht api & games card

---
+++ components/Main.jsx
@@ -0,0 +1,153 @@
+import { useEffect, useState } from "react";
+import { getLatestGames } from "../lib/metacritic";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { View, ActivityIndicator, FlatList } from "react-native";
+import { Logo } from "../components/Logo";
+
+import { GameCard, AnimatedGameCard } from "./GameCard";
+// import Constants from "expo-constants";
+
+export function Main() {
+  const [dataGames, setDataGames] = useState([]);
+  const insets = useSafeAreaInsets();
+
+  const getData = async () => {
+    const data = await getLatestGames();
+    setDataGames(data);
+    // console.log("DataGames:", JSON.stringify(data, null, 2));
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  // return <CardInfo dataGames={dataGames} />;
+
+  return (
+    <View style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}>
+      <View style={{ marginBottom: 15 }}>
+        <Logo />
+      </View>
+
+      {/* SafeAreaView Solo para IOS */}
+      {/* <SafeAreaView style={{ marginVertical: 10 }}> */}
+
+      {dataGames.length === 0 ? (
+        <ActivityIndicator size="large" color="#fff" />
+      ) : (
+        <FlatList
+          data={dataGames}
+          keyExtractor={(game) => game.slug}
+          renderItem={({ item, index }) => (
+            <AnimatedGameCard game={item} index={index} />
+          )}
+        />
+
+        // <ScrollView>
+        //   {dataGames.map((game) => (
+        //     <GameCard key={game.slug} game={game} />
+        //   ))}
+        // </ScrollView>
+      )}
+      {/* </SafeAreaView> */}
+    </View>
+  );
+
+  // return (
+  //   <View style={styles.container}>
+  //     {/* Componente especial de expo para mostrar el estado del dispositivo */}
+  //     <StatusBar style="light" />
+
+  //     {/* <Image
+  //       blurRadius={4}
+  //       source={icon}
+  //       style={{ width: 100, height: 100, resizeMode: 'center' }}
+  //     />
+  //     <Image
+  //       fadeDuration={600}
+  //       source={icon}
+  //       style={{ width: 200, height: 100, resizeMode: 'contain' }}
+  //     />
+  //     */}
+  //     <Image
+  //       source={icon}
+  //       style={{ width: 200, height: 100, resizeMode: "stretch" }}
+  //     />
+
+  //     <Image
+  //       // blurRadius={4}
+  //       source={{
+  //         uri: "https://www.metacritic.com/a/img/catalog/provider/6/12/6-1-767900-52.jpg",
+  //       }}
+  //       style={{ width: 200, height: 200, resizeMode: "contain" }}
+  //     />
+  //     <Text style={{ color: "white" }}>
+  //       Open up App.js to start working on your app!
+  //     </Text>
+  //     {/* Boton nativo no se puede estilar */}
+  //     <Button color="" title="Press me" onPress={() => alert("Hello")} />
+
+  //     <TouchableHighlight
+  //       // Color que tendra detras
+  //       underlayColor={"#29f"}
+  //       style={{
+  //         backgroundColor: "#fff",
+  //         padding: 10,
+  //         borderRadius: 10,
+  //         marginTop: 10,
+  //         width: 100,
+  //         height: 100,
+  //         alignItems: "center",
+  //         justifyContent: "center",
+  //       }}
+  //       onPress={() => alert("Hello")}
+  //     >
+  //       <Text>Boton personalizado</Text>
+  //     </TouchableHighlight>
+
+  //     <Pressable
+  //       onPress={() => {
+  //         try {
+  //           getData();
+  //           alert("Datos obtenidos");
+  //         } catch (error) {
+  //           console.error(error);
+  //         }
+  //       }}
+  //       style={{
+  //         backgroundColor: "#fff",
+  //         padding: 10,
+  //         marginTop: 10,
+  //         width: 100,
+  //         height: 100,
+  //         alignItems: "center",
+  //         justifyContent: "center",
+  //         borderRadius: 100,
+  //       }}
+  //     >
+  //       <Text style={{ color: "black" }}>Obtener datos de jeugos</Text>
+  //     </Pressable>
+
+  //     <Pressable
+  //       onPress={() => console.log(dataGame)}
+  //       style={({ pressed }) => [
+  //         {
+  //           backgroundColor: pressed ? "rgb(210, 230, 255)" : "white",
+  //         },
+  //         styles.wrapperCustom,
+  //       ]}
+  //     >
+  //       {({ pressed }) => (
+  //         <Text
+  //           style={{
+  //             fontSize: pressed ? 32 : 16,
+  //             color: pressed ? "red" : "black",
+  //           }}
+  //         >
+  //           {pressed ? "Pressed!" : "mostrar dataGame"}
+  //         </Text>
+  //       )}
+  //     </Pressable>
+  //   </View>
+  // );
+}
